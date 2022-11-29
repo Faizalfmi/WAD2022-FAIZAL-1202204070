648a69e378c6eb1bafd528283a553e4c85939c4c
alfianmf <alfianm999@gmail.com>
2022-11-29 08:10:09 +0700

    debug

--- MODUL4_FAIZAL/pages/Login.php
+++ MODUL4_FAIZAL/pages/Login.php
@@ -1,30 +1,30 @@
-<?php 
-if (!isset($_SESSION)) {
-    session_start();
-}
+        <?php 
+        if (!isset($_SESSION)) {
 
-require 'connector.php';
-$email = $_POST['email'];
-$password = $_POST['sandi'];
+        }
 
-$data = "SELECT * FROM user_faizal WHERE email='$email' && password='$password'";
-$query = mysqli_query($conn, $data);
+        require 'connector.php';
+        $email = $_POST['email'];
+        $password = $_POST['sandi'];
 
-if (mysqli_num_rows($query) > 0) {
-    $result = mysqli_fetch_assoc($query);
+        $data = "SELECT * FROM user_faizal WHERE email='$email' && password='$password'";
+        $query = mysqli_query($conn, $data);
 
-    
-        $_SESSION['email'] = $result['email'];
-        $_SESSION['message'] = 'Anda berhasil login!';
-        header('location:Home-Faizal.php');
-        exit();
-    } else {
-        $_SESSION['message-error'] = 'Password anda salah, silahkan coba lagi';
+        if (mysqli_num_rows($query) > 0) {
+            $result = mysqli_fetch_assoc($query);
+
+            
+                $_SESSION['email'] = $result['email'];
+                $_SESSION['message'] = 'Anda berhasil login!';
+                header('location:Home-Faizal.php');
+                exit();
+            } else {
+                $_SESSION['message-error'] = 'Password anda salah, silahkan coba lagi';
+                header('location:Login-Faizal.php');
+                exit();
+            }
+
+        $_SESSION['message-error'] = 'Gagal Login';
         header('location:Login-Faizal.php');
         exit();
-    }
-
-$_SESSION['message-error'] = 'Gagal Login';
-header('location:Login-Faizal.php');
-exit();
-?>
+        ?>
